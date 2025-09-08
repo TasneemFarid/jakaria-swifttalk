ce51a15c446c537418e7006b91af9db43f76d201
TasneemFarid <tasneemfarid.webdev@gmail.com>
2025-09-08 12:07:06 +0600

    ok

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router'
-import SideBar from '../Components/sideBar'
+import SideBar from '../Components/sidebar'
 
 const RootLayout = () => {
   return (
